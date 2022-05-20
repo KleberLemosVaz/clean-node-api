8f61628b33af48f558d4612e7e4b60023a6f4d81
Kleber Lemos Vaz <kleberlemosvaz@Klebers-MacBook-Pro.local>
2022-05-20 13:57:30 -0300

    refactor: move EmailValidator creation to a helper method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -7,13 +7,26 @@ interface SutTypes {
     emailValidatorStub: EmailValidator
 }
 
-const makeSut = (): SutTypes => {
+const makeEmailValidator = (): EmailValidator => {
     class EmailValidatorStub implements EmailValidator {
         isValid (email: string): boolean {
             return true
         }
     }
-    const emailValidatorStub = new EmailValidatorStub()
+    return new EmailValidatorStub()
+}
+
+const makeEmailValidatorWithError = (): EmailValidator => {
+    class EmailValidatorStub implements EmailValidator {
+        isValid (email: string): boolean {
+            throw new Error()
+        }
+    }
+    return new EmailValidatorStub()
+}
+
+const makeSut = (): SutTypes => {
+    const emailValidatorStub = makeEmailValidator()
     const sut = new SignUpController(emailValidatorStub)
     return {
         sut,
@@ -105,17 +118,12 @@ describe('SignUp Controller', () => {
                 passwordConfirmation: 'any_password'
             }
         }
-         sut.handle(httpRequest)
+        sut.handle(httpRequest)
         expect(isValidSpy).toHaveBeenCalledWith('any_email@email.com')
     })
 
     test('Should return 500 if EmailValidator throws', () => {
-        class EmailValidatorStub implements EmailValidator {
-            isValid (email: string): boolean {
-                throw new Error()
-            }
-        }
-        const emailValidatorStub = new EmailValidatorStub()
+        const emailValidatorStub = makeEmailValidatorWithError()
         const sut = new SignUpController(emailValidatorStub)
         const httpRequest = {
             body: {
